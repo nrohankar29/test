e79b25de7e09b8817fd701741f4e0f7bb024aebf
Git Test <gittest@linuxacademy.com>
2017-05-25 07:29:19 +0000

    test script to display Environment Variables

---
+++ env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear
+
+echo "This script will give us environment information"
+echo "================================================"
+echo ""
+echo "Hello Username: $USER"
+echo ""
+echo "Your Home Directory is: $HOME"
+echo ""
+echo "Your History File WIll Ignore: $HISTCONTROL"
+echo ""
+echo "Your Terminal session type is: $TERM"
+echo ""
