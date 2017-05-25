d94aebdb242185611b3eab0b014ac59162ec8b74
Git Test <gittest@linuxacademy.com>
2017-05-25 08:25:01 +0000

    Setting and using Variables in script

---
+++ varexample.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+MYUSERNAME="username"
+MYPASSWORD="password123"
+STARTOFSCRIPT=`date`
+echo ""
+echo "My login name for this application is: $MYUSERNAME"
+echo ""
+echo "My login password for this application is: $MYPASSWORD"
+echo""
+echo "I started this script at: $STARTOFSCRIPT"
+echo ""
+
+ENDOFSCRIPT=`date`
+
+echo "I ended this script at: $ENDOFSCRIPT"
+echo ""
+
+
