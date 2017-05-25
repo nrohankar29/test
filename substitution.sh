845fa9846ded770ae8285f260fe9ad548be9dde7
Git Test <gittest@linuxacademy.com>
2017-05-25 09:15:16 +0000

    Sample script for command substitution

---
+++ substitution.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This script is intended to show how to do simple substitution
+
+shopt -s expand_aliases
+
+alias TODAY="date"
+alias UFILES="find /home -user user"
+
+TODAYSDATE=`date`
+USERFILES=`find /home -user user`
+
+echo "Today's Date: $TODAYSDATE"
+echo "All Files owned by USER: $USERFILES"
+
+A=`TODAY`
+B=`UFILES`
+
+echo "With Alias, Today is: $A"
+echo "With Alias, UFILES is: $B"
+
+
