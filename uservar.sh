6dba64b1945b5bfa5cca4d2395a9602da603ba1a
Git Test <gittest@linuxacademy.com>
2017-05-26 09:50:15 +0000

    Use of user defined variables

---
+++ uservar.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This script shows the use of user defined variables 
+
+clear
+STARTOFSCRIPT=`date`
+
+MYUSERNAME="James"
+
+echo "Your user name is: $MYUSERNAME"
+
+MYPASSWORD="bond007"
+
+echo ""
+
+echo "Your password is: $MYPASSWORD"
+
+echo ""
+
+echo "This script started at: $STARTOFSCRIPT"
+
+ENDOFSCRIPT=`date`
+
+echo ""
+
+echo "This script ended at: $ENDOFSCRIPT"
+
+echo ""
